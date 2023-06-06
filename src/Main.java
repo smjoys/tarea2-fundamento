6238ff122b54e2d27d51a3b8c1037ec1e8dd5394
smjoys <iefh00@gmail.com>
2023-06-06 12:45:19 -0400

    1

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner tcld= new Scanner(System.in);
+        System.out.println("Ingrese nombre del archivo (Subtitulos):");
+        String arch= tcld.next();
+
+
+        FileReader archivo;
+        BufferedReader lector;
+
+        try {
+            archivo=new FileReader(arch);
+            lector = new BufferedReader(archivo);
+            String linea;
+            LinkedList<String> list= new LinkedList<>();
+            while ((linea = lector.readLine())!=null){
+                list.add(linea);
+            }
+            if (list!=null){
+                for (int i=0;i<list.size();i++){
+                    String num = list.get(i);
+                    StringTokenizer numer= new StringTokenizer(num, "-->");
+                    String fl = new String(numer.nextToken());
+
+
+                }
+            }
+            lector.close();
+            archivo.close();
+        } catch (IOException e) {
+            System.out.println("trayectorias No encontradas");
+        }
+    }
+}
