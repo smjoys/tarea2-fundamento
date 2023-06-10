630f1839bf632486c2102855a8d90fdfbfb4c09c
MatiasFlores <mafloresgu@gmail.com>
2023-06-10 19:02:13 -0400

    2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Subtitulo> Subtitulos = new ArrayList<>();
 
 
         Scanner tcld= new Scanner(System.in);
